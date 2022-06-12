d83b2f71dbaa13ea7dcb7d95628405b5f8d7e873
NayaRPL <naya.mmj@gmail.com>
2022-06-12 12:56:52 +0800

    hapus data

---
+++ hapus.php
@@ -0,0 +1,7 @@
+<?php 
+include 'config.php';
+$id = $_GET['id'];
+mysqli_query($koneksi, "DELETE FROM mahasiswa WHERE id='$id'")or die(mysqli_error());
+ 
+header("location:index.php?pesan=hapus");
+?>

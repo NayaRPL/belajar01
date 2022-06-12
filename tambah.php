b8c47601bbea6a5af898387ea731fc950176e3c5
NayaRPL <naya.mmj@gmail.com>
2022-06-12 10:30:14 +0800

    w

---
+++ tambah.php
@@ -0,0 +1,13 @@
+<?php 
+include 'config.php';
+
+$nama= $_POST['nama'];
+$nim = $_POST['nim'];
+$angkatan= $_POST['angkatan'];
+$alamat = $_POST['alamat'];
+
+mysqli_query($koneksi,"insert into mahasiswa values('','$nama','$nim','$angkatan','$alamat')");
+
+header("location:index.php");
+
+?>
